ad9fa08f4531294867ce5a57f0dae9246a7bed66
maiconjobim <49690103+maiconjobim@users.noreply.github.com>
2019-10-10 12:23:44 -0300

    Create Calendario com struct.c

---
+++ Calendario com struct.c
@@ -0,0 +1,50 @@
+# include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+# include <string.h>
+	#define h 10
+
+	 
+ void main()
+ {
+ 	
+ 	
+ 	
+ 		struct Data
+		{
+			int mes;
+			int dia,ano;
+			
+		}data_nascim;
+		
+		
+		
+		
+		data_nascim.ano =2015;
+		FILE*p;
+		p= fopen("Novocalendario.TXT","w");
+		
+		for(int i=0 ;i<31;i++){	
+	
+		
+			data_nascim.dia = i+1;
+			for(int j=0;j<12;j++){
+				
+				data_nascim.mes= j+1;
+				
+				if ((data_nascim.mes == 2 && data_nascim.dia >28) || (data_nascim.mes == 4 && data_nascim.dia >30) || (data_nascim.mes == 6 && data_nascim.dia >30) ||(data_nascim.mes == 9 && data_nascim.dia >30) || (data_nascim.mes == 11 && data_nascim.dia >30) ){
+					
+				fprintf(p,"\t\t");
+					
+				}else fprintf(p,"%5d/%d/%d\t",data_nascim.dia,data_nascim.mes,data_nascim.ano);	
+				
+			}
+			
+		//printf("\n");
+		fprintf(p,"\n");
+		} 
+		
+	fclose(p);
+
+ }
