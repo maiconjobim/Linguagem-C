a976c61e7e2619b8c9b61a09b04eca71a4327f53
maiconjobim <49690103+maiconjobim@users.noreply.github.com>
2019-11-12 20:33:33 -0300

    Update pnteiros2.c

--- pnteiros2.c
+++ pnteiros2.c
@@ -7,7 +7,8 @@ setlocale  (LC_ALL,"");
 int *ptr1,*ptr2;
 int n1 =5 , n2 =200;
 
-// * int n2 =200,n1=5;  Uando este para trocar a ordem de declaração das variáveirs
+ n2 =5;
+ n1=200;  //Uando este para trocar a ordem de declaração das variáveirs
 
 ptr1 = &n1;
 
@@ -18,16 +19,16 @@ if(ptr1>ptr2)
     printf("CASO 1: A CONDIÇÃO É VERDADEIRA \n\n");
     printf("COM USO DE PONTEIRO\n\n");
     printf("O valor endereço de n1 é maior do que de n2 ; \n\n ");
-    printf("O endereço de n1 (n1 este que vale %d) é igual a %d(em decimal)\n",ptr1,*ptr1);
+    printf("O endereço de n1 (n1 este que vale %d) é igual a %d(em decimal)\n",*ptr1,ptr1);
     printf("ou %p (em hexadecimal)\n",ptr1);
     
-    printf("O endereço de n2 (n2 este que vale %d) é igual a %d(em decimal)\n",ptr2,ptr2);
+    printf("O endereço de n2 (n2 este que vale %d) é igual a %d(em decimal)\n",*ptr2,ptr2);
     printf("ou %p (em hexadecimal)\n \n \n",ptr2);
     
     printf("SEM USO DE PONTEIROS \n\n");
    
     printf("O valor endereço de n1 é maior do que de n2 ; \n\n ");
-    printf("O endereço de n1 (n1 este que vale %d) é igual a %d(em decimal)\n",n1, n1);
+    printf("O endereço de n1 (n1 este que vale %d) é igual a %d(em decimal)\n",n1, &n1);
     printf("ou %p (em hexadecimal)\n",&n1);
     
     printf("O endereço de n2 (n2 este que vale %d) é igual a %d(em decimal)\n",n2,&n2);
